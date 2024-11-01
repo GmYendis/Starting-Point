5c7286f59949693d6b09f6f1a20b80c757ad6704
GmYendis <jotmudi@gmail.com>
2024-10-31 20:53:55 -0600

    1st try I'm trying get the hang of this.

---
+++ AIO Python/hello.py
@@ -0,0 +1,14 @@
+# About to build my first app here wish me luck
+
+
+# This variable contains an integer
+quantity = 14
+
+# This variable contains a float
+unit_price = 26.99
+
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+
+# Show the results
+print(extended_price)
